873014ed787f429fddda509357c3d5b6a234904f
flameking <2711554770@qq.com>
2022-05-30 20:09:59 +0800

    ui布局架构初步完成

---
+++ src/components/MessageList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="message-list">
+    <!-- 1.聊天窗口 -->
+    <div class="message">
+      <!-- 3.消息体 -->
+      <div class="message-body">
+        <!-- 3-1.时间戳 -->
+        <p></p>
+        <div>
+          <!-- 3-2.消息 -->
+          <p></p>
+          <!-- 3-3.用户头像 -->
+          <!-- <img src="../assets/logo.png" alt="用户名"/> -->
+        </div>
+      </div>
+    </div>
+    <!-- 2.消息发送窗口 -->
+    <div class="message-send">
+      
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "MessageList",
+
+  data() {
+    return {};
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
